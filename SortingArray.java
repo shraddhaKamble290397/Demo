270bd76dd9817531483f91624e1f3f536b5bc116
akshaydate38 <112371760+akshaydate38@users.noreply.github.com>
2022-09-15 15:55:38 +0530

    Add files via upload

---
+++ SortingArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class SortingArray {
+
+	public static void main(String args[]) {
+		int[]a = {99,50,45,20,4,22};
+		for(int i=0; i<a.length; i++) {
+			for(int j=i+1; j<a.length; j++) {
+			if(a[i]>a[j]) {
+				
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+			System.out.println(a[i]+" ");
+		}
+	}
+}
