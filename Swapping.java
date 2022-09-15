6a8c9f1f80683fd88c6bddc2deb7e6310bda85a9
shraddhaKamble290397 <113573718+shraddhaKamble290397@users.noreply.github.com>
2022-09-15 15:31:23 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+package Simple;
+
+public class Swapping {
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=50;
+		int c=0;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("a="+a+"b="+b);
+	}
+
+}
