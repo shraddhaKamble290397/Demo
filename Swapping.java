f8c54e856b00d80e053ad5d334fa40d55721e94e
shraddhaKamble290397 <113573718+shraddhaKamble290397@users.noreply.github.com>
2022-09-15 15:38:18 +0530

    Update Swapping.java
    changes made in Integer values

--- Swapping.java
+++ Swapping.java
@@ -3,8 +3,9 @@ package Simple;
 public class Swapping {
 	public static void main(String args[])
 	{
-		int a=10;
-		int b=50;
+		int a=20;
+		int b=80;
+		//(Changes made By Shraddha)
 		int c=0;
 		c=a;
 		a=b;
